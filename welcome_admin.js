b6c02c3614135dff2cf14fc769fa2b53afd8e7ea
Ilyes IA22 <ilyes@example.com>
2026-02-22 01:16:01 -0500

    💎 SECRET: Easter Egg Souverain injecté

--- welcome_admin.js
+++ welcome_admin.js
@@ -8,3 +8,16 @@
         alert("Noyau IA22 : Bienvenue chez vous, Maître.");
     }
 })();
+
+// Protocole Easter Egg Secret
+let inputSecret = "";
+window.addEventListener('keydown', (e) => {
+    inputSecret += e.key.toUpperCase();
+    if (inputSecret.includes("IA22")) {
+        document.body.style.transition = "all 1s";
+        document.body.style.background = "radial-gradient(circle, #ff0000, #000000)";
+        alert("SOUVERAINETÉ DZROUGE CONFIRMÉE. BIENVENUE, MAÎTRE ZOUBIROU.");
+        inputSecret = "";
+    }
+    if (inputSecret.length > 10) inputSecret = "";
+});
